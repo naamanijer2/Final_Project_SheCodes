6d791c93470f30362668020adad03e9ba04604c3
naamanijer2 <naamanijer2@gmail.com>
2021-12-13 13:06:18 +0200

    Create main function that get the input from the user

---
+++ Main.py
@@ -0,0 +1,4 @@
+
+def main():
+    medical_restriction = input("Please choose restriction number: 1 - Sugar-Free, 2 - Gluten-Free, 3 - Dairy-Free ")
+    favorite_ingredients = input("Please Enter up to 2 favorite ingredients: (for example - egg, tomato) ")
