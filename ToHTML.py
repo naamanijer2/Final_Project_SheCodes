67fc79e0cc5372efe8a211de6e2db601ab3fc0d2
naamanijer2 <naamanijer2@gmail.com>
2022-01-31 21:11:49 +0200

    create HTMLanf form for the restrictions

--- ToHTML.py
+++ ToHTML.py
@@ -1,21 +1,35 @@
 from flask import Flask
 from flask import *
 from functools import wraps
+from flask import Flask, render_template, request
+import cgi
 import sqlite3
 
 #class ToHTML:
 
+app = Flask(__name__)
 
- #   app = Flask(__name__)
+@app.route('/')
+#def home():
+#    title = 'Recipes'
+#    return render_template('home.html', title = title)
 
-  #  @app.route('/')
-   # def home(self):
-    #    return render_template('home.html')
+@app.route('/res/')
+def chooseInput():
+    title = 'choose input'
+    #form = cgi.FieldStorage()
+    #restriction = form.getvalue('Please choose restriction number: 1 - Sugar-Free, 2 - Gluten-Free, 3 - Dairy-Free')
+    #return restriction
+    restriction = request.form.get("restriction")
+    return render_template('home.html', title = title, restriction = restriction)
 
-    #@app.route('/generate')
-    #def generate(self):
-     #   print('Hello World!')
-      #  return render_template('process.html')
+    #@app.route('/hi/')
+    #def who(self):
+     #   return "Who are you?"
 
-#if __name__ == '__main__':
- #   ToHTML().home()
+    #@app.route('/hi/<username>')
+    #def greet(self, username):
+     #   return f"Hi there, {username}!"
+
+if __name__ == '__main__':
+    app.run()
